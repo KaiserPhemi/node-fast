0d0daa09b9335455cf5454650a84404b5955594f
Oluwafemi Akinwa <kaiser.phemi@protonmail.ch>
2025-10-05 10:07:13 +0100

    chore()

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,21 @@
 import Fastify from "fastify";
+import dotenv from "dotenv";
 
+dotenv.config();
+
+const port = process.env.PORT
 const fastify = Fastify({
   logger: true,
 });
 
-try {
-   fastify.listen({ port: 5555 });
-} catch (error) {
-  fastify.log.error(error);
-  process.exit(1);
+async function main() {
+  try {
+    await fastify.listen({ port: port ? parseInt(port) : 5555 });
+    fastify.log.info(`Server listening on port ${port}`);
+  } catch (err) {
+    fastify.log.error(err);
+    process.exit(1);
+  }
 }
+
+main();
